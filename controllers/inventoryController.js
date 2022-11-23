09bd883e94a0445a1bb41d7b4c358a1fa31bf603
wilsonn-g <wilson-gee@hotmail.com>
2022-11-23 07:02:09 -0800

    patch api for an inventory item

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,11 +1,39 @@
-const knex = require('knex')(require('../knexfile'))
+const knex = require("knex")(require("../knexfile"));
+
+/*
+ * @param object representing req.body
+ * @return boolean on whether any of the values were null
+ */
+function validateBody(request) {
+  for (const property in request) {
+    if (!request[property]) return false;
+  }
+  return true;
+}
 
 exports.index = (_req, res) => {
-  knex('inventories')
+  knex("inventories")
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories; ${err}`)
-    )
-}
+    .catch((err) => res.status(400).send(`Error retrieving Inventories; ${err}`));
+};
+
+exports.update = (req, res) => {
+  console.log("got here");
+  if (!validateBody(req.body)) {
+    return res
+      .status(400)
+      .send("Bad request: please ensure none of the fields (name, description, category, status, warehouse) are empty");
+  }
+  const { id } = req.params;
+  knex("inventories")
+    .where({ id: id })
+    .update(req.body)
+    .then((_data) => {
+      knex("inventories")
+        .where({ id: id })
+        .then((data) => res.status(200).json(data[0]));
+    })
+    .catch((err) => res.status(500).send(`Error while updating item ${id}: ${err}`));
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,5 +1,7 @@
-const router = require('express').Router()
-const inventoryController = require('../controllers/inventoryController')
+const router = require("express").Router();
+const inventoryController = require("../controllers/inventoryController");
 
-router.route('/').get(inventoryController.index)
-module.exports = router
+router.route("/").get(inventoryController.index);
+
+router.route("/:id").patch(inventoryController.update);
+module.exports = router;
