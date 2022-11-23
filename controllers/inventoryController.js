0e121c3e8af809375a5bd857efbdd976512b7877
wilsonn-g <wilson-gee@hotmail.com>
2022-11-23 06:15:56 -0800

    Add inventory API with uuid package

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,11 +1,37 @@
-const knex = require('knex')(require('../knexfile'))
+const { v4: uuidv4 } = require("uuid");
+const knex = require("knex")(require("../knexfile"));
 
-exports.index = (_req, res) => {
-  knex('inventories')
-    .then((data) => {
-      res.status(200).json(data)
-    })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Inventories; ${err}`)
-    )
+/*
+ * @param object representing req.body
+ * @return boolean on whether any of the values were null
+ */
+function validateBody(request) {
+  for (const property in request) {
+    if (!request[property]) return false;
+  }
+  return true;
 }
+exports.index = (_req, res) => {
+  knex("inventories")
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).send(`Error retrieving Inventories; ${err}`));
+};
+
+exports.add = (req, res) => {
+  if (!validateBody(req.body)) {
+    return res
+      .status(400)
+      .send("Bad request: please ensure none of the fields (name, description, category, status, warehouse) are empty");
+  }
+  const newItemId = uuidv4();
+  knex("inventories")
+    .insert({ ...req.body, id: newItemId })
+    .then((_data) => {
+      knex("inventories")
+        .where({ id: newItemId })
+        .then((data) => res.status(201).json(data[0]));
+    })
+    .catch((err) => res.status(500).send(`Error while adding new inventory item to database: ${err}`));
+};
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
-const inventoryController = require('../controllers/inventoryController')
+const router = require("express").Router();
+const inventoryController = require("../controllers/inventoryController");
 
-router.route('/').get(inventoryController.index)
-module.exports = router
+router.route("/").get(inventoryController.index).post(inventoryController.add);
+
+module.exports = router;
