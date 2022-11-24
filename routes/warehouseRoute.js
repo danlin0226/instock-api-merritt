5a7f9b2a1f713f105c61d85e26b6a78c5baba2db
Allan Somera <nagal.somera@gmail.com>
2022-11-24 02:27:49 -0800

    fixed some formatting issues

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,12 +1,12 @@
-const router = require('express').Router()
+const router = require('express').Router();
 
-const warehouseController = require('../controllers/warehouseController')
+const warehouseController = require('../controllers/warehouseController');
 
-router.route('/').get(warehouseController.index)
+router.route('/').get(warehouseController.index);
 router
   .route('/:id')
   .get(warehouseController.singleWarehouse)
-  .delete(warehouseController.remove)
-router.route('/:id/inventories').get(warehouseController.warehouseInventories)
+  .delete(warehouseController.remove);
+router.route('/:id/inventories').get(warehouseController.warehouseInventories);
 
-module.exports = router
+module.exports = router;
