e34874cd5604e34dec0ce04067b33838576729db
danlin0226 <danlin0226@gmail.com>
2022-11-23 16:25:11 -0800

    update get inventory by warehouseID with a join

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,35 +1,36 @@
-const knex = require('knex')(require('../knexfile'))
+const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
-    .catch((err) => res.status(400).send(`Error retrieving Warehouse ${err}`))
-}
+    .catch((err) => res.status(400).send(`Error retrieving Warehouse ${err}`));
+};
 
 exports.singleWarehouse = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .where({ id: req.params.id })
     .then((data) => {
       if (!data.length) {
         return res
           .status(404)
-          .send(`Record with id: ${req.params.id} is not found`)
+          .send(`Record with id: ${req.params.id} is not found`);
       }
       // Knex returns an array of records, so we need to send response with a single object only
-      res.status(200).json(data[0])
+      res.status(200).json(data[0]);
     })
     .catch((err) =>
       res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
-    )
-}
+    );
+};
 
 exports.warehouseInventories = (req, res) => {
-  knex('inventories')
+  knex("warehouses")
+    .join("inventories", "inventories.warehouse_id", "warehouses.id")
     .where({ warehouse_id: req.params.id })
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch((err) =>
       res
@@ -37,5 +38,5 @@ exports.warehouseInventories = (req, res) => {
         .send(
           `Error retrieving inventories for Warehouse ${req.params.id} ${err}`
         )
-    )
-}
+    );
+};
