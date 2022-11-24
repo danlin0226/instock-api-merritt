d18beeaa9012e13e13612b4f2fa6b9d9c5ea498c
Allan Somera <nagal.somera@gmail.com>
2022-11-24 02:00:25 -0800

    added endpoint for delete

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -1,9 +1,12 @@
-const router = require('express').Router();
+const router = require('express').Router()
 
-const warehouseController = require('../controllers/warehouseController');
+const warehouseController = require('../controllers/warehouseController')
 
-router.route('/').get(warehouseController.index);
-router.route('/:id').get(warehouseController.singleWarehouse);
-router.route('/:id/inventories').get(warehouseController.warehouseInventories);
+router.route('/').get(warehouseController.index)
+router
+  .route('/:id')
+  .get(warehouseController.singleWarehouse)
+  .delete(warehouseController.remove)
+router.route('/:id/inventories').get(warehouseController.warehouseInventories)
 
-module.exports = router;
+module.exports = router
