34615d0f7dd52cf9ea12b8917af26e8766ea5eca
super_fox <super_fox@mail.ru>
2022-11-23 23:15:27 -0800

    warehouse add func working

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,7 +1,8 @@
-const knex = require('knex')(require('../knexfile'));
+const { v4: uuidv4 } = require("uuid");
+const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .then((data) => {
       res.status(200).json(data);
     })
@@ -9,7 +10,7 @@ exports.index = (_req, res) => {
 };
 
 exports.singleWarehouse = (req, res) => {
-  knex('warehouses')
+  knex("warehouses")
     .where({ id: req.params.id })
     .then((data) => {
       if (!data.length) {
@@ -26,8 +27,8 @@ exports.singleWarehouse = (req, res) => {
 };
 
 exports.warehouseInventories = (req, res) => {
-  knex('warehouses')
-    .join('inventories', 'inventories.warehouse_id', 'warehouses.id')
+  knex("warehouses")
+    .join("inventories", "inventories.warehouse_id", "warehouses.id")
     .where({ warehouse_id: req.params.id })
     .then((data) => {
       res.status(200).json(data);
@@ -38,5 +39,64 @@ exports.warehouseInventories = (req, res) => {
         .send(
           `Error retrieving inventories for Warehouse ${req.params.id} ${err}`
         )
+    );
+};
+
+exports.addWarehouse = (req, res) => {
+  console.log(req.body, "body");
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+
+    // warehouse_name ||
+    //   !req.body.address ||
+    //   !req.body.city ||
+    //   !req.body.country ||
+    //   !req.body.contact_name ||
+    //   !req.body.contact_position ||
+    //   !req.body.contact_phone ||
+    //   !req.body.email
+  ) {
+    return res
+      .status(400)
+      .send("Please provide details about warehouse and it's contacts!");
+  }
+
+  const newWarehouseId = uuidv4();
+  knex("warehouses")
+    .insert({ ...req.body, id: newWarehouseId })
+    .then((_data) => {
+      knex("warehouses")
+        .where({ id: newWarehouseId })
+        .then((data) => {
+          res.status(201).json(data[0]);
+        });
+    })
+    .catch((err) =>
+      res.status(400).send(`Error creating new Warehouse: ${err}`)
+    );
+};
+
+exports.editWarehouse = (req, res) => {
+  knex("warehouses")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then((_data) => {
+      knex("warehouses")
+        .where({ id: req.params.id })
+        .then((data) => {
+          res.status(200).json(data[0]);
+        });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error updating Warehouse with ID: ${req.params.id} ${err}`)
     );
 };
--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -2,8 +2,16 @@ const router = require('express').Router();
 
 const warehouseController = require('../controllers/warehouseController');
 
-router.route('/').get(warehouseController.index);
-router.route('/:id').get(warehouseController.singleWarehouse);
+router
+.route('/')
+.get(warehouseController.index)
+.post(warehouseController.addWarehouse);
+
+router
+.route('/:id')
+.get(warehouseController.singleWarehouse)
+.patch(warehouseController.editWarehouse);
+
 router.route('/:id/inventories').get(warehouseController.warehouseInventories);
 
 module.exports = router;
