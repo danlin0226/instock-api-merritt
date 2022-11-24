d11d88f716964e978b209ec04f510e39b4a535c1
Allan Somera <nagal.somera@gmail.com>
2022-11-24 02:22:46 -0800

    fixed semicolons from disappearing

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,12 +1,12 @@
-const knex = require('knex')(require('../knexfile'))
+const knex = require('knex')(require('../knexfile'));
 
 exports.index = (_req, res) => {
   knex('warehouses')
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
-    .catch((err) => res.status(400).send(`Error retrieving Warehouse ${err}`))
-}
+    .catch((err) => res.status(400).send(`Error retrieving Warehouse ${err}`));
+};
 
 exports.singleWarehouse = (req, res) => {
   knex('warehouses')
@@ -15,22 +15,22 @@ exports.singleWarehouse = (req, res) => {
       if (!data.length) {
         return res
           .status(404)
-          .send(`Record with id: ${req.params.id} is not found`)
+          .send(`Record with id: ${req.params.id} is not found`);
       }
       // Knex returns an array of records, so we need to send response with a single object only
-      res.status(200).json(data[0])
+      res.status(200).json(data[0]);
     })
     .catch((err) =>
       res.status(400).send(`Error retrieving warehouse ${req.params.id} ${err}`)
-    )
-}
+    );
+};
 
 exports.warehouseInventories = (req, res) => {
   knex('warehouses')
     .join('inventories', 'inventories.warehouse_id', 'warehouses.id')
     .where({ warehouse_id: req.params.id })
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch((err) =>
       res
@@ -38,19 +38,19 @@ exports.warehouseInventories = (req, res) => {
         .send(
           `Error retrieving inventories for Warehouse ${req.params.id} ${err}`
         )
-    )
-}
+    );
+};
 
 exports.remove = (req, res) => {
   knex('warehouses')
     .delete()
     .where({ id: req.params.id })
     .then((data) => {
-      res.status(200).json(data)
+      res.status(200).json(data);
     })
     .catch((err) =>
       res
         .status(400)
         .send(`Error warehouse ${req.params.id} does not exists ${err}`)
-    )
-}
+    );
+};
